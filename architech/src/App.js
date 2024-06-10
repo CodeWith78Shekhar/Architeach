14c74f792fbf790c7dc6431f806118f6a49d9655
coder shekhar <shekharsharma0119@gmail.com>
2024-06-10 15:20:08 +0530

    "changes"

--- architech/src/App.js
+++ architech/src/App.js
@@ -1,18 +1,10 @@
 import React from 'react'
-<<<<<<< HEAD
-import Header from './components/Header/Header.jsx'
-const App = () => {
-  return (
-    <>
-     <Header/>
-=======
 import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <>
       <Footer/>
->>>>>>> 984c82bb415f8b10bb9273bf983e7862dd3eda19
     </>
   )
 }
